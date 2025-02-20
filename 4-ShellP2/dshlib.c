ea2da27d5a658d735cfed851b6ccc1785bb99a2e
godofthecrabz <cfschr8@aol.com>
2025-02-20 14:46:20 -0500

    gettin close to done

--- 4-ShellP2/dshlib.c
+++ 4-ShellP2/dshlib.c
@@ -68,10 +68,12 @@ int exec_local_cmd_loop()
 
     char *cmd_buff = malloc(sizeof(char) * SH_CMD_MAX);
     int rc = 0;
+    int forkRet = 0;
     cmd_buff_t cmd;
+    bool shouldExit = false;
     alloc_cmd_buff(&cmd);
 
-    while (1) {
+    while (!shouldExit) {
         printf("%s", SH_PROMPT);
         if (fgets(cmd_buff, ARG_MAX, stdin) == NULL) {
             printf("\n");
@@ -79,34 +81,38 @@ int exec_local_cmd_loop()
         }
         cmd_buff[strcspn(cmd_buff,"\n")] = '\0';
 
-        if (strcmp(cmd_buff, EXIT_CMD) == 0) {
-            break;
-        } else if (strcmp(cmd_buff, "") == 0) {
-            printf(CMD_WARN_NO_CMD);
-        } else if (strcmp(cmd_buff, "dragon") == 0) {
-            print_dragon();
-        } else {
-            clear_cmd_buff(&cmd);
-            rc = build_cmd_buff(cmd_buff, &cmd);
+        clear_cmd_buff(&cmd);
+        rc = build_cmd_buff(cmd_buff, &cmd);
 
-            switch (rc) {
-                case OK:
-                    if (strcmp(cmd.argv[0], "cd") == 0) {
-                        if (cmd.argc > 1) {
-                            chdir(cmd.argv[1]);
-                        }
-                    }
-                    break;
-                case ERR_TOO_MANY_COMMANDS:
-                    printf(CMD_ERR_PIPE_LIMIT, CMD_MAX);
-                    break;
-                case ERR_CMD_OR_ARGS_TOO_BIG:
-                    printf("error: executable name or arguments are too long\n");
-                    break;
-                default:
-                    printf("SHOULD NOT REACH HERE\n");
-                    break;
-            }
+        switch (rc) {
+            case OK:
+                Built_In_Cmds ret = exec_built_in_cmd(&cmd);
+                switch (ret) {
+                    case BI_EXECUTED:
+                        break;
+                    case BI_CMD_EXIT:
+                        shouldExit = true;
+                        break;
+                    case BI_RC:
+                        printf("%d\n", forkRet);
+                        break;
+                    case BI_NOT_BI:
+                        forkRet = exec_cmd(&cmd);
+                        break;
+                    default:
+                        printf("SHOULD NOT REACH HERE\n");
+                        break;
+                }
+                break;
+            case ERR_TOO_MANY_COMMANDS:
+                printf(CMD_ERR_PIPE_LIMIT, CMD_MAX);
+                break;
+            case ERR_CMD_OR_ARGS_TOO_BIG:
+                printf("error: executable name or arguments are too long\n");
+                break;
+            default:
+                printf("SHOULD NOT REACH HERE\n");
+                break;
         }
     }
 
@@ -143,19 +149,25 @@ int clear_cmd_buff(cmd_buff_t *cmd_buff) {
 int build_cmd_buff(char *cmd_line, cmd_buff_t *cmd_buff) {
     int cmdCount = 0;
 
+    if (strcmp(cmd_line, "") == 0) {
+        cmd_buff->_cmd_buffer[0] = '\0';
+        cmd_buff->argv[0] = cmd_buff->_cmd_buffer;
+    }
+
     char *tok = strtok(cmd_line, PIPE_STRING);
     while (tok) {
         int argCount = 0;
         int cmdLen = 0;
         int start = 0;
         bool spaceLast = true;
+        bool includeSpace = false;
 
         while (*tok) {
             if (cmdCount == CMD_MAX) {
                 return ERR_TOO_MANY_COMMANDS;
             }
             if (*tok == SPACE_CHAR) {
-                if (!spaceLast) {
+                if (!spaceLast && !includeSpace) {
                     cmd_buff->_cmd_buffer[cmdLen] = '\0';
                     cmd_buff->argv[argCount] = cmd_buff->_cmd_buffer + start;
                     cmdLen++;
@@ -167,7 +179,11 @@ int build_cmd_buff(char *cmd_line, cmd_buff_t *cmd_buff) {
                 if (cmdLen >= SH_CMD_MAX - 1) {
                     return ERR_CMD_OR_ARGS_TOO_BIG;
                 }
-                cmd_buff->_cmd_buffer[cmdLen] = *tok;
+                if (*tok == '"') {
+                    includeSpace = !includeSpace;
+                } else {
+                    cmd_buff->_cmd_buffer[cmdLen] = *tok;
+                }
                 spaceLast = false;
                 cmdLen++;
             }
@@ -193,11 +209,58 @@ Built_In_Cmds match_command(const char *input) {
     } else if (strcmp(input, "rc") == 0) {
         return BI_RC;
     } else if (strcmp(input, "") == 0) {
+        return BI_EXECUTED;
+    } else {
+        return BI_NOT_BI;
     }
 }
 
 Built_In_Cmds exec_built_in_cmd(cmd_buff_t *cmd) {
+    Built_In_Cmds bi_cmd = match_command(cmd->argv[0]);
+    switch (bi_cmd) {
+        case BI_CMD_EXIT:
+            return BI_CMD_EXIT;
+        case BI_CMD_DRAGON:
+            print_dragon();
+            return BI_EXECUTED;
+        case BI_CMD_CD:
+            if (cmd->argc > 1) {
+                chdir(cmd->argv[1]);
+            }
+            return BI_EXECUTED;
+        case BI_RC:
+            return BI_RC;
+        case BI_NOT_BI:
+            return BI_NOT_BI;
+        case BI_EXECUTED:
+            return BI_EXECUTED;
+        default:
+            printf("Error executing built in command\n");
+            exit(ERR_EXEC_CMD);
+    }
 }
 
 int exec_cmd(cmd_buff_t *cmd) {
+    int f_result, c_result;
+
+    f_result = fork();
+    if (f_result < 0) {
+        perror("fork error");
+        exit(ERR_EXEC_CMD);
+    }
+
+    if (f_result == 0) {
+        int rc = execvp(cmd->argv[0], cmd->argv);
+        if (rc < 0) {
+            perror("fork error");
+            exit(ERR_EXEC_CMD);
+        }
+    } else {
+        int rc = wait(&c_result);
+        if (rc < f_result) {
+            perror("fork error");
+            exit(ERR_EXEC_CMD);
+        }
+        return c_result;
+    }
 }
